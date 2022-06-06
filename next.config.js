9036928b4948843c8576ab08bd8fa93f23dcc97e
umidjon-2231 <83820489+umidjon-2231@users.noreply.github.com>
2022-06-06 19:08:36 +0500

    fix bug

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@
 const nextConfig = {
   env: {
     // SERVER_URL: "https://fastfood-uz.herokuapp.com/api/",
-    SERVER_URL: "http://localhost:8080/api/",
+    // SERVER_URL: "http://localhost:8080/api/",
     // SERVER_HOST_URL: "https://fastfood-uz.herokuapp.com"
-    SERVER_HOST_URL: "http://localhost:8080"
+    // SERVER_HOST_URL: "http://localhost:8080"
   },
   images: {
     domains: ['fastfood-uz.herokuapp.com', "localhost"]
